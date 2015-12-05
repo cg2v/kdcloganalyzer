105e6aa33f632a042da177f8dfa6a98aea0c1591
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2015-12-05 14:03:49 -0500

    Record reader: Organize imports, add copyright, patterns
    clean up nused imports, add a license to KDCLogRecordReader
    add the patterns that will be used to pick out the interesting log lines

--- src/main/java/edu/cmu/cc/hadoop/kerberos/loganalyzer/KDCLogFileInputType.java
+++ src/main/java/edu/cmu/cc/hadoop/kerberos/loganalyzer/KDCLogFileInputType.java
@@ -16,13 +16,10 @@
 package edu.cmu.cc.hadoop.kerberos.loganalyzer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.hadoop.fs.FileStatus;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
--- src/main/java/edu/cmu/cc/hadoop/kerberos/loganalyzer/KDCLogRecordReader.java
+++ src/main/java/edu/cmu/cc/hadoop/kerberos/loganalyzer/KDCLogRecordReader.java
@@ -1,11 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Chaskiel Grundman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package edu.cmu.cc.hadoop.kerberos.loganalyzer;
 
 import java.io.IOException;
-
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,13 +27,40 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.Seekable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.compress.*;
+import org.apache.hadoop.io.compress.CodecPool;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.Decompressor;
+import org.apache.hadoop.io.compress.SplitCompressionInputStream;
+import org.apache.hadoop.io.compress.SplittableCompressionCodec;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.util.LineReader;
 
 public class KDCLogRecordReader extends
 		RecordReader<LongWritable, KDCLogRecord> {
-	 private static final Log LOG = LogFactory.getLog(KDCLogRecordReader.class);
+	private static final Log LOG = LogFactory.getLog(KDCLogRecordReader.class);
+
+	private final static String TIMESTAMP = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}";
+	private final static String ATOM = "[/-_\\.a-zA-Z0-9]+";
+	private final static String REALM = "[/-\\.a-zA-Z0-9]+";
+	private final Pattern matchInitial = Pattern.compile("(" + TIMESTAMP +
+			")\\s+((?:AS|TGS)-REQ)\\s+(" + ATOM +
+			")@(" + REALM + ")\\s+from\\s+(IPv4:[\\d\\.]+|IPv6:[\\p{XDigit}.:])+for\\s+(" + ATOM +
+	                       ")@(" + REALM + ")");
+	private final String matchPreauthSuccess = "\\bPre-authentication succeeded\\b";
+	private final String matchBadPassword = "\\bFailed to decrypt PA-DATA --";
+	private final String matchBadClient = "\\bUNKNOWN --";
+	private final String matchExpiredClient = "\\bClient expired\\b";
+	private final String matchTimeSkew = "\\bToo large time skew";
+	private final String matchNoPreauthKey = "\\bNo key matches pa-data";
+	private final String matchBadTGS = "\bkrb_rd_req:";
+	private final Pattern matchSending = Pattern.compile(TIMESTAMP +
+			"\\s+sending\\s+\\d+\\s+bytes\\s+to\\s+" +
+	                       "(?:IPv4:[\\d\\.]+|IPv6:[\\p{XDigit}.:])");
+
     private CompressionCodecFactory compressionCodecs = null;
     private long start;
     private long pos;
