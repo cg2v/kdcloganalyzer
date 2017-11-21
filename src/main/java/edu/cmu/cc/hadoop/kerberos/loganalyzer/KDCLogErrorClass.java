2013a4b982d3ee221ed64fa5f1dc3e6d6a29945c
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2017-11-21 13:05:46 -0500

    Include KDCLogErrorClass
    Include new file KDCLogErrorClass.java that belongs with
    c1389d85

---
+++ src/main/java/edu/cmu/cc/hadoop/kerberos/loganalyzer/KDCLogErrorClass.java
@@ -0,0 +1,11 @@
+package edu.cmu.cc.hadoop.kerberos.loganalyzer;
+
+public enum KDCLogErrorClass {
+NO_ERROR,
+BAD_NAME,
+UNUSABLE_NAME,
+BAD_PASSWORD,
+BAD_PARAMETERS,
+BAD_AUTHENTICATION,
+UNKNOWN
+}
